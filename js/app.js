703cd6640e233314a3320508e6f4c18e53b174d5
shafaana6 <khaalidashafaana@gmail.com>
2026-01-02 17:27:31 +0530

    Day 2: Custom video controls added

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const video = document.getElementById("videoPlayer");
+const playPauseBtn = document.getElementById("playPauseBtn");
+const progressBar = document.getElementById("progressBar");
+const timeDisplay = document.getElementById("timeDisplay");
+
+// PLAY / PAUSE
+playPauseBtn.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    playPauseBtn.textContent = "⏸️";
+  } else {
+    video.pause();
+    playPauseBtn.textContent = "▶️";
+  }
+});
+
+// UPDATE PROGRESS BAR
+video.addEventListener("timeupdate", () => {
+  const progress = (video.currentTime / video.duration) * 100;
+  progressBar.value = progress;
+
+  const minutes = Math.floor(video.currentTime / 60);
+  const seconds = Math.floor(video.currentTime % 60)
+    .toString()
+    .padStart(2, "0");
+
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+});
+
+// SEEK VIDEO
+progressBar.addEventListener("input", () => {
+  const seekTime = (progressBar.value / 100) * video.duration;
+  video.currentTime = seekTime;
+});
+
